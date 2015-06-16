993fb60fc4dac406b786cef7ff56d6876bd803e2
yinzhelu <yinzhelu@utexas.edu>
2015-06-16 09:55:11 -0500

    Working Fast Mutex code
    Got Fast Mutex algorithm to work. We may need to debug my bakery
    algorithm implementation separately to see why it isn't working.

--- HW1Q1/q1/src/FastMutexLock.java
+++ HW1Q1/q1/src/FastMutexLock.java
@@ -8,29 +8,40 @@ public class FastMutexLock implements MyLock {
 	protected volatile boolean [] flag; 
     public FastMutexLock(int numThread) {
       // TODO: initialize your algorithm
-      this.X = -1;
-      this.Y = -1;
+      this.X = -1; 
+      this.Y = -1; // doorway open
       this.flag = new boolean [numThread];
-      Arrays.fill(this.flag, Boolean.FALSE);
+      Arrays.fill(this.flag, Boolean.TRUE);
     }
 
     @Override
     public void lock(int myId) {
       // TODO: the locking algorithm
       while (true){
-    	  flag[myId] = true;
+    	  flag[myId] = false;
     	  X = myId;
-    	  if(Y != -1){
-    		  flag[myId] = false;
-    		  while(Y != -1);
+    	  if(Y != -1){ // Check if door is still open
+    		  flag[myId] = true;
+    		  while(Y != -1); // Wait until door open to reenter lock
     		  continue;
     	  }
     	  else{
-    		  Y = myId;
-    		  if (X == myId)
+    		  Y = myId; 
+    		  if (X == myId){ // Check if going "Down", Fast path
     			  return;
-    		  else{
-    			  flag[myId] = false;
+    		  }
+    		  else{ // Going "Right"
+    			  flag[myId] = true;
+    			  for (int j = 0; j < flag.length; j++)
+    			  {
+    				  while (flag[j] != true);
+    			  }
+    			  if(Y == myId) // Slow Path. Slowest of process to simultaneously access Right returns
+    				  return;
+    			  else{
+    				  while(Y != -1); // Check door is open. Reenter if that's the case.
+    				  continue;
+    			  }
     		  }
     	  }
       }
@@ -40,6 +51,6 @@ public class FastMutexLock implements MyLock {
     public void unlock(int myId) {
       // TODO: the unlocking algorithm
       Y = -1;
-      flag[myId] = false;
+      flag[myId] = true;
     }
 }
