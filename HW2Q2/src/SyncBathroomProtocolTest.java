c4a2b15c2d0c503c49b6c0fd2767b448a947511a
triskadecaepyon <dcltechnology@gmail.com>
2015-06-21 10:33:54 -0500

    Adding of JUnit tests (Q2)

---
+++ HW2Q2/src/SyncBathroomProtocolTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidliu on 6/21/15.
+ */
+public class SyncBathroomProtocolTest {
+
+    @Test
+    public void testEnterMale() throws Exception {
+
+    }
+
+    @Test
+    public void testLeaveMale() throws Exception {
+
+    }
+
+    @Test
+    public void testEnterFemale() throws Exception {
+
+    }
+
+    @Test
+    public void testLeaveFemale() throws Exception {
+
+    }
+}
