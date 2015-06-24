5a5f5ee3820d6dfbe54fb3300bc8e41c37aa583f
yinzhelu <yinzhelu@utexas.edu>
2015-06-24 12:56:47 -0500

    Tentative Problem 2 solution
    Based off David's solution for part 1.

--- HW2Q2/src/SyncBathroomProtocol.java
+++ HW2Q2/src/SyncBathroomProtocol.java
@@ -2,22 +2,62 @@
 // Use synchronized, wait(), notify(), and notifyAll() to implement this
 // bathroom protocol
 
-public class SyncBathroomProtocol implements BathroomProtocol {
+public class SyncBathroomProtocol extends BathroomProtocol {
+
+  int male_count = 0;
+  int female_count = 0;
+  int male_queue = 0;
+  int female_queue = 0;
 
   public void enterMale() {
-
+	  synchronized(this){
+		  if(female_count > 0 || (female_queue > male_queue && male_count==0))
+			  male_queue++;
+		  while(female_count > 0 || female_queue > male_queue){
+			  try{
+				  wait();
+			  }
+			  catch(Exception e){System.out.println(e);}
+		  }
+		  male_count++;
+	  }
   }
 
   public void leaveMale() {
-
+	  synchronized(this){
+	        if (male_count > 0) {
+	            //Safe to step out
+	            male_count--;
+	        }
+	        else
+	        {
+	        	notifyAll();
+	        }
+	  }
   }
 
   public void enterFemale() {
-
+	  if(male_count > 0 || (male_queue > female_queue && female_count==0))
+		 female_queue++;
+	  while(male_count > 0 || male_queue > female_queue){
+		  try{
+			  wait();
+		  }
+		  catch(Exception e){System.out.println(e);}
+	  }
+	  female_count++;
   }
 
   public void leaveFemale() {
-
+	  synchronized(this){
+	        if (female_count > 0) {
+	            //Safe to step out
+	            female_count--;
+	        }
+	        else {
+	        notifyAll();
+	        }
+	  }
   }
 
 
