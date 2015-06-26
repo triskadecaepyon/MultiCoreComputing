aaa8a34301d315cc481dc0b251811c20c6eeb6ca
yinzhelu <yinzhelu@utexas.edu>
2015-06-25 19:11:23 -0500

    Ugly solution
    At least no starvation/deadlock

--- HW2Q2/src/SyncBathroomProtocol.java
+++ HW2Q2/src/SyncBathroomProtocol.java
@@ -8,82 +8,95 @@ public class SyncBathroomProtocol implements BathroomProtocol {
     int female_count = 0;
     int male_queue = 0;
     int female_queue = 0;
-    boolean restroom_access = true;
 
-
+    boolean male_restroom_access = true;
+    boolean female_restroom_access = false;
+    
+    boolean male_turn = true;
+    boolean female_turn = true;
+    int male_total = 0;
+    int female_total = 0;
+    
     public void enterMale() {
         synchronized(this) {
-            while(!restroom_access) {
+        	male_queue++;
+  //          if(male_queue > female_queue){
+  //          	female_turn = false;
+  //          }
+            while(female_count > 0 || (female_turn &&  !male_turn && !male_restroom_access)) {
+              //female_count > 0 || female_queue > male_queue
               try {
-                  System.out.println("Male having to wait");
                   wait();
-              }
+              }             
               catch(Exception e){
                   e.printStackTrace();}
             }
-            restroom_access = false;
+            female_restroom_access = false;
             System.out.println("Not waiting anymore (enterMale)");
-            if(female_count > 0 || (female_queue > male_queue && male_count==0))
-              male_queue++;
-            else {
+            female_turn = true;
+            male_turn = false;
               male_count++;
-            }
-            System.out.println("EM: " + male_count + " " + male_queue);
-            //If you can enter, clear our queue
-            male_queue = 0;
-            restroom_access = true;
-            notifyAll();
+              male_queue--;
+              male_total++;
+            System.out.println("EM: " + male_count + " " + male_queue + " total male " + male_total);
         }
     }
 
     public void leaveMale() {
         synchronized(this){
             System.out.println("Leave (LeaveMale)");
-            if (male_count > 0) {
+            male_count--;
+            if (male_count == 0) {
                 //Safe to step out
-                male_count--;
+            	System.out.println("Notify all");
+            	female_restroom_access = true;
+            	male_restroom_access = false;
+            	notifyAll();
             }
+ //           female_restroom_access = true;
             System.out.println("EM: " + male_count + " " + male_queue);
-            restroom_access = true;
-            notifyAll();
         }
     }
 
     public void enterFemale() {
         synchronized (this) {
-            while (!restroom_access) {
+        	female_queue++;
+            if(female_queue > male_queue){
+            	male_turn = false;
+            }
+            System.out.println("Female access: " + female_turn + "Male access: " + male_turn);
+            while (male_count > 0 || (male_turn && !female_turn && !female_restroom_access)) {
                 try {
-                    System.out.println("Female having to wait");
                     wait();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            restroom_access = false;
+            male_restroom_access = false;
+ //           female_restroom_access = true;
             System.out.println("Not waiting anymore (enterFemale)");
-            if (male_count > 0 || (male_queue > female_queue && female_count == 0))
-                female_queue++;
-            else {
-                female_count++;
-            }
-            System.out.println("FM: " + female_count + " " + female_queue);
-            restroom_access = true;
-            notifyAll();
+            female_turn = false;
+            male_turn = true;
+            female_count++; 
+            female_queue--;
+            female_total++;
+            
+            System.out.println("FM: " + female_count + " " + female_queue + " total female " + female_total);
         }
     }
 
     public void leaveFemale() {
       synchronized(this){
-            System.out.println("Leave (LeaveFemale)");
-            if (female_count > 0) {
-                //Safe to step out
-                female_count--;
+            System.out.println("LeaveFemale" + " female count: " + female_count);
+            female_count--;
+            if (female_count == 0) {
+            	System.out.println("Notifying everyone!");
+            	male_restroom_access = true;
+            	female_restroom_access = false;
+                notifyAll();
             }
+ //           male_restroom_access = true;
             System.out.println("FM: " + female_count + " " + female_queue);
-            //If you can enter, clear our queue
-            female_queue = 0;
-            restroom_access = true;
-            notifyAll();
       }
     }
 
