b55de5207b0202d60dea3347e6c4f0e32deb56f2
triskadecaepyon <dcltechnology@gmail.com>
2015-07-04 23:11:09 -0500

    Initial Matrix_mult starter code

--- HW3Q1/matrix_mult.c
+++ HW3Q1/matrix_mult.c
@@ -7,7 +7,33 @@
 #include <omp.h>
 #include <stdio.h>
 
-int main() {
-    printf("hello world");
-    printf("Number of processors: %d", omp_get_num_procs());
+int main( int argc, char *argv[] ) {
+    // Determine if all the necessary arguments are there
+    if (argc < 3) {
+        printf("Need all 3 arguments");
+        return 0;
+    } else {
+        printf("File 1: %s, ", argv[1]);
+        printf("File 2: %s, ", argv[2]);
+        printf("Threads: %s \n", argv[3]);
+    }
+
+    FILE *file_one = fopen( argv[1], "r" );
+    if ( file_one == 0 )
+        {
+            printf( "Issue opening first file\n" );
+        } else {
+            int x;
+            // Temporary file reading system placeholder
+            while  ( ( x = fgetc( file_one ) ) != EOF ) {
+                printf( "%c", x );
+            }
+            fclose( file_one );
+    }
+#pragma omp parallel
+    {
+        printf("hello world test\n");
+        printf("Number of processors: %d \n", omp_get_num_procs());
+    }
+    return 0;
 }
