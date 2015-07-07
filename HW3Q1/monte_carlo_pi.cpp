ca908ad26c4a044ae1aa8699288647ca372b8c6f
triskadecaepyon <dcltechnology@gmail.com>
2015-07-06 23:28:56 -0500

    Formatting of Monte Carlo Method

--- HW3Q1/monte_carlo_pi.cpp
+++ HW3Q1/monte_carlo_pi.cpp
@@ -8,22 +8,24 @@ double MonteCarloPi(int s) {
 // TODO: Implement your Monte Carlo Method
 // parameter s: number of points you randomly choose
 // return the value of pi
-   double x,y;
-   int i,count=0; /* # of points in the 1st quadrant of unit circle */
-   double pi;
-   long seed = 35791246;
+    double x, y;
+    int i, count = 0; /* # of points in the 1st quadrant of unit circle */
+    double pi;
+    long seed = 35791246;
 
-   /* initialize random numbers */
-   srand(seed);
-   count=0;
-   #pragma omp for schedule(static) // Schedule parallel threads for random point generation
-   for ( i=0; i<s; i++) {
-      x = (double)rand()/RAND_MAX; 
-      y = (double)rand()/RAND_MAX;
-      if (x*x+y*y<=1) count++; // Determine if points are within circle
-   
-   pi=(double)count/s*4; // Calculate Pi based on number of points in circle
-   return pi;
+    /* initialize random numbers */
+    srand(seed);
+    count = 0;
+#pragma omp for schedule(static) // Schedule parallel threads for random point generation
+    for (i = 0; i < s; i++) {
+        x = (double) rand() / RAND_MAX;
+        y = (double) rand() / RAND_MAX;
+        if (x * x + y * y <= 1) count++; // Determine if points are within circle
+
+        pi = (double) count / s * 4; // Calculate Pi based on number of points in circle
+        return pi;
+
+    }
 }
 
 int main(){
