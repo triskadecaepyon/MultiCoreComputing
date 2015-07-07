d1f76e3d3a5405a74bcf5f963b455f390139dd47
triskadecaepyon <dcltechnology@gmail.com>
2015-07-07 00:20:35 -0500

    Working Monte Carlo code!!

--- HW3Q1/monte_carlo_pi.cpp
+++ HW3Q1/monte_carlo_pi.cpp
@@ -1,39 +1,43 @@
 #include "omp.h"
-#include <stdio.h>
-#include <time.h>
-#include <random>
 #include <stdlib.h>
+#include <stdio.h>
+#define SEED 35791246
 
 double MonteCarloPi(int s) {
 // TODO: Implement your Monte Carlo Method
 // parameter s: number of points you randomly choose
 // return the value of pi
+    int niter = s;
     double x, y;
     int i, count = 0; /* # of points in the 1st quadrant of unit circle */
+    double z;
     double pi;
-    long seed = 35791246;
 
     /* initialize random numbers */
-    srand(seed);
+    srand(SEED);
     count = 0;
-#pragma omp for schedule(static) // Schedule parallel threads for random point generation
-    for (i = 0; i < s; i++) {
-        x = (double) rand() / RAND_MAX;
-        y = (double) rand() / RAND_MAX;
-        if (x * x + y * y <= 1) count++; // Determine if points are within circle
+#pragma omp parallel shared(chunk) private(i,x,y,z) reduction(+:count)
+    {
 
-        pi = (double) count / s * 4; // Calculate Pi based on number of points in circle
-        return pi;
-
+#pragma omp for schedule(dynamic,chunk)
+        for (i = 0; i < niter; i++) {
+            x = (double) rand() / RAND_MAX;
+            y = (double) rand() / RAND_MAX;
+            z = x * x + y * y;
+            if (z <= 1) count++;
+        }
     }
+    pi = (double) count / niter * 4;
+    printf("# of trials= %d , estimate of pi is %g \n", niter, pi);
+    return pi;
 }
 
-int main(){
-	int s = 10000;
-	printf("Enter s: ");
-	scanf("%d", &s);
-	printf("S is %d\n", s);
-	double pi = MonteCarloPi(s);
-	printf("Pi is %lf.\n", pi);
-	return 0;
+int main() {
+    int s = 50000;
+    printf("Enter s: ");
+    scanf("%d", &s);
+    printf("S is %d\n", s);
+    double pi = MonteCarloPi(s);
+    printf("Pi is %lf.\n", pi);
+    return 0;
 }
