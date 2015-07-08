e705e41fa7b5287da85108ee3383b0cc549349ae
Yinzhe Lu <ylu@localhost.localdomain>
2015-07-07 19:50:32 -0500

    add comments

--- HW3Q1/matrix_mult_new.c
+++ HW3Q1/matrix_mult_new.c
@@ -12,9 +12,10 @@
 #include <cstdlib>
 
 void print_matrix(double *matrix, int m, int n){
+// Function displays the resultant matrix in commandline
 	for (int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-		printf("%f ", matrix[i*n+j]);
+		printf("%.1f ", matrix[i*n+j]);
 	    }
             printf("\n");
 	}
@@ -33,6 +34,7 @@ double* C, int T) {
 // parameter C: indicates the matrix C, which is the results of A x B
 // parameter T: indicates the number of threads
 // return true if A and B can be multiplied; otherwise, return false
+	double sum = 0;
         printf("In function\n");
         int i, j, k;
 	if(colA != rowB) // incompatible matrix dimensions!
@@ -40,19 +42,19 @@ double* C, int T) {
 	   printf("Incompatible Matrix Dimensions!\n");
 	   return false;
 	}
-	#pragma omp parallel num_threads(T) shared(A, B, C) private(i, j, k)
+	#pragma omp parallel num_threads(T) shared(A, B, C) private(i, j, k, sum) //run process in parallel
 	{
 	#pragma omp for schedule(static)
 		for (i = 0 ; i < rowA; i++)
 		{
 			for(j = 0; j < colB; j++)
 			{
-		   		double sum = 0;
 		   		for (k = 0; k < colA; k++) 
 				{
-	          			 sum = sum + A[i*colA+k] * B[k*colB+j];
+	          			 sum += A[i*colA+k] * B[k*colB+j]; // sum up multiplied pairs for C[i][j]
         			}
-			   	C[i*colB+j] = sum;
+				C[i*colB+j] = sum; //Update C[i][j]
+				sum = 0; // clear sum variable for reuse
 			}
 		}
 	}
@@ -63,12 +65,12 @@ void matrixPop(int row, int col, double* matrix, char* arg){
 
     std::string line;
     std::ifstream infileOne(arg);
-    std::getline(infileOne, line);
+    std::getline(infileOne, line); // skip first line
     for(int i = 0; i < row; i++){
         std::getline(infileOne, line);
         std::istringstream iss(line);
         for(int j = 0; j < col; j++){
-	   iss >> matrix[i*col+j];
+	   iss >> matrix[i*col+j]; // populate matrix array with corresponding element from file
 	}
     }
     return;
@@ -100,29 +102,29 @@ int main( int argc, char *argv[] ) {
     std::getline(infileOne, line);
     std::istringstream iss_one(line);
 
-    T = atoi(argv[3]);
+    T = atoi(argv[3]); // convert thread number from string to int
     if (!(iss_one >> ROWA >> COLA)) { 
-	printf("Something bad happened!");
+	printf("Something bad happened!"); //Invalid file
 	return 0; 
     } // error
     printf("%d, %d\n", ROWA, COLA);
-    A = new double[ROWA*COLA];
-    matrixPop(ROWA, COLA, A, argv[1]);
-    print_matrix(A, ROWA, COLA);
+    A = new double[ROWA*COLA]; //dynamically create matrix A
+    matrixPop(ROWA, COLA, A, argv[1]); //populate matrix A
+//    print_matrix(A, ROWA, COLA);
 
     std::getline(infileTwo, line);
     std::istringstream iss_two(line);
 
     if (!(iss_two >> ROWB >> COLB)) { 
-	printf("Something bad happened!");
+	printf("Something bad happened!"); //invalid file
 	return 0; 
     } // error
 
-    B = new double[ROWB*COLB];
-    matrixPop(ROWB, COLB, B, argv[2]);
-    print_matrix(B, ROWB, COLB);
-    C = new double[ROWA*COLB];
-    MatrixMult(ROWA, COLA, A, ROWB, COLB, B, C, T);
-    print_matrix(C, ROWA, COLB);
+    B = new double[ROWB*COLB]; //dynamically create matrix B
+    matrixPop(ROWB, COLB, B, argv[2]); //populate matrix B
+//    print_matrix(B, ROWB, COLB);
+    C = new double[ROWA*COLB]; //dynamically create destination matrix
+    MatrixMult(ROWA, COLA, A, ROWB, COLB, B, C, T); //Matrix multiplication
+    print_matrix(C, ROWA, COLB); //Display matrix result
     return 0;
 }
