d24baa5e71669cf06f4a77b0f4ca5a540ffde8c4
triskadecaepyon <dcltechnology@gmail.com>
2015-07-15 22:15:04 -0500

    Adding of Coarse Lock implementation, basic tests
    Note that this has not been tested with multiple threads.

--- HW4Q2/src/CoarseGrainedListSet.java
+++ HW4Q2/src/CoarseGrainedListSet.java
@@ -1,12 +1,93 @@
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CoarseGrainedListSet<T> implements ListSet<T> {
-  public boolean add(T value) {
-    return false;
-  }
-  public boolean remove(T value) {
-    return false;
-  }
-  public boolean contains(T value) {
-    return false;
-  }
+    private Node head;
+    ReentrantLock listlock;
+
+    public CoarseGrainedListSet() {
+        this.head = new Node(Integer.MIN_VALUE);
+        this.head.next = new Node(Integer.MAX_VALUE);
+        this.listlock = new ReentrantLock();
+    }
+
+    public boolean add(T value) {
+        Node pred, curr;
+        int key = value.hashCode();
+        listlock.lock();
+
+        try {
+            pred = head;
+            curr = pred.next;
+            while (curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            if (key == curr.key) {
+                return false;
+            } else {
+                Node node = new Node(value);
+                node.next = curr;
+                pred.next = node;
+                return true;
+            }
+        } finally {
+            listlock.unlock();
+        }
+    }
+
+    public boolean remove(T value) {
+        Node pred, curr;
+        int key = value.hashCode();
+        listlock.lock();
+
+        try {
+            pred = head;
+            curr = pred.next;
+            while (curr.key < key) {
+                pred = curr;
+                curr = curr.next;
+            }
+            if (key == curr.key) {
+                pred.next = curr.next;
+                return true;
+            } else {
+                return false;
+            }
+        } finally {
+            listlock.unlock();
+        }
+    }
+
+    public boolean contains(T value) {
+        Node pred, curr;
+        int key = value.hashCode();
+        listlock.lock();
+        try {
+            pred = head;
+            curr = pred.next;
+            while (curr.next != null && key != curr.key) {
+                //System.out.println(curr.key + " " + key);
+                curr = curr.next;
+            }
+            if (key == curr.key) {
+                //System.out.println("Found: " + value);
+                return true;
+            }
+        } finally {
+            listlock.unlock();
+        }
+        //System.out.println("Not Found: " + value);
+        return false;
+    }
+}
+
+class Node<T> {
+    T item;
+    int key;
+    Node next;
+
+    public Node(T t) {
+        item = t;
+        key = item.hashCode();
+    }
 }
--- HW4Q2/src/CoarseGrainedListSetTest.java
+++ HW4Q2/src/CoarseGrainedListSetTest.java
@@ -1,8 +1,49 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by davidliu on 7/12/15.
  */
+
 public class CoarseGrainedListSetTest {
 
+    @Test
+    public void BasicSetTest() {
+        CoarseGrainedListSet myset = new CoarseGrainedListSet();
+
+        myset.add(4);
+        myset.add(3);
+        assertTrue(myset.contains(3));
+    }
+
+    @Test
+    public void BasicAddTest() {
+        CoarseGrainedListSet myset = new CoarseGrainedListSet();
+
+        myset.add(4);
+        assertTrue(myset.contains(4));
+    }
+
+    @Test
+    public void RemovalSimple() {
+        CoarseGrainedListSet myset = new CoarseGrainedListSet();
+
+        myset.add(4);
+        myset.add(3);
+        myset.remove(3);
+        assertFalse(myset.contains(3));
+    }
+
+    @Test
+    public void RemoveAdvanced() {
+        CoarseGrainedListSet myset = new CoarseGrainedListSet();
+
+        myset.add(4);
+        myset.add(3);
+        myset.remove(4);
+        myset.remove(3);
+        myset.add(3);
+        assertTrue(myset.contains(3));
+    }
 }
