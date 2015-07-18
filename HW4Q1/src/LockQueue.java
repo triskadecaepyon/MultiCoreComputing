ddf8e0c51c4736af2b93431ca08dc2adf6c933eb
yinzhelu <yinzhelu@utexas.edu>
2015-07-18 15:07:40 -0500

    update locked queue
    tentative solution

--- HW4Q1/src/LockQueue.java
+++ HW4Q1/src/LockQueue.java
@@ -1,8 +1,52 @@
+import java.util.concurrent.locks.ReentrantLock ;
+
 public class LockQueue<T> implements MyQueue<T> {
+  ReentrantLock enqLock , deqLock;
+  Node<T> head;
+  Node<T> tail;
+  public class Node<T> {
+
+	    private T data;
+	    private Node<T> next;
+
+	    public Node(T data){
+	        this.data = data;
+	        this.next = null;
+	    }
+  }
+  
+  public LockQueue(){
+	  head = new Node<T>(null);
+	  tail = head;
+	  enqLock = new ReentrantLock();
+	  deqLock = new ReentrantLock();
+  }
   public boolean enq(T value) {
-    return false;
+	  if( value == null ) throw new NullPointerException();
+	  enqLock.lock( ) ;
+	  try{ 
+		  Node<T> e = new Node<T>(value) ;
+		  tail.next = e ;
+		  tail = e;
+	  }
+	  finally{
+		  enqLock.unlock();
+  	  }
+      return false;
   }
   public T deq() {
-    return null;
+	T result;
+	deqLock.lock();
+	try{
+		while(head.next == null ) {
+		}
+		result = head.next.data;
+		head = head.next;
+	}
+	finally{
+		deqLock.unlock();
+	}
+	
+    return result;
   }
 }
