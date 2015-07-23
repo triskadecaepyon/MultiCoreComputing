ee4315706a69992056dba53b87ac7e43dc57ea3c
yinzhelu <yinzhelu@utexas.edu>
2015-07-23 17:30:47 -0500

    return null and add test case
    Return null instead of continuing if queue is empty.
    
    Adding test case.

--- HW4Q1/src/LockFreeQueue.java
+++ HW4Q1/src/LockFreeQueue.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
@@ -60,12 +62,11 @@ public class LockFreeQueue<T> implements MyQueue<T> {
             if (first == head.getReference()) {
                 if (first == last) {
                     if (next == null) {
-                        continue;
+                        return null;//continue;
                     }
                     tail.compareAndSet(last, next, tail_mark[0], tail_mark[0] + 1);
                 } else {
                     T result = next.data;
-                    System.out.println("result available");
                     if (head.compareAndSet(first, next, head_mark[0], head_mark[0] + 1)) {
                         return result;
                     }
@@ -75,11 +76,65 @@ public class LockFreeQueue<T> implements MyQueue<T> {
     }
 
     public static void main(String args[]) {
-        LockFreeQueue<Integer> myQueue = new LockFreeQueue<Integer>(12);
-        System.out.println(myQueue.enq(20));
-        System.out.println(myQueue.enq(35));
-        System.out.println(myQueue.enq(12));
-        System.out.println(myQueue.deq());
-        // System.out.println(myQueue.deq());
+        LockFreeQueue<Integer> q = new LockFreeQueue<Integer>(12);
+ /*       System.out.println(q.deq());
+        System.out.println(q.enq(20));
+        System.out.println(q.enq(35));
+        System.out.println(q.enq(12));
+        System.out.println(q.deq());
+        */
+
+        class queueThread extends Thread {
+			int totalNum;
+			int dice;
+			volatile LockFreeQueue<Integer> q;
+			Random rand;
+			Random diceRand;
+
+			queueThread(int n, LockFreeQueue<Integer> q) {
+				totalNum = n;
+				this.q = q;
+				rand = new Random();
+				diceRand = new Random();
+			}
+
+			public void run() {
+				for (int i = 0; i < totalNum; i++) {
+					int value = 0;
+					dice = diceRand.nextInt(100);
+					try{
+						if (dice > 30) {
+							value = rand.nextInt(100);
+							q.enq(value);
+						} else {
+							System.out.println(q.deq());
+						}
+					}catch(IllegalStateException e){
+						System.out.println("EMPTY!!");
+					}
+					//System.out.println("i: " + i + "\tdice: "+ dice + "\tvalue: "+ value);
+				}
+			}
+		}
+
+		ArrayList<queueThread> threadPool = new ArrayList<queueThread>();
+	    int threadNum = 10;
+	    int operationNum = 200;
+		//	System.out.println("perThread:" + perThread);
+		for (int i = 0; i < threadNum ; i++) {
+			int perThread = operationNum / threadNum;
+			threadPool.add(new queueThread(perThread , q));
+		}
+		for (int i = 0; i < threadNum; i++) {
+			threadPool.get(i).start();
+		}
+		int i = 0;
+		try {
+			for (i = 0; i < threadNum; i++) {
+				threadPool.get(i).join();
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Thread " + i + " is interrupted");
+		}
     }
 }
